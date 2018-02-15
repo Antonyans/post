6c28b3b5612e86e674bfb9d2b8e11176a7362f17
Sasun <sasun.31@mail.ru>
2018-02-15 20:16:06 +0300

    fix2

--- apps/register/views.py
+++ apps/register/views.py
@@ -1,18 +1,21 @@
 from django.contrib import auth
 from django.contrib.auth import authenticate
 from django.shortcuts import render, redirect
-
+from django.http import HttpResponseRedirect
 from apps.register.forms import UserForm
+from django.urls import reverse
+
 
 
 # Create your views here.
 
 def signup(request):
-    form = UserForm()
-    if request.POST and form.is_valid():
-        form = UserForm(request.POST or None)
-        form.save()
-        return render(request, 'posts.html')
+    if request.POST:
+        form = UserForm( request.POST)
+        if request.POST and form.is_valid():
+            form = UserForm(request.POST or None)
+            form.save()
+            return render(request, 'posts.html', locals())
     else:
         form = UserForm()
         return render(request, 'registration/signup.html', {'form': form})
@@ -26,7 +29,7 @@ def signin(request):
         user = authenticate(username=username, password=password)
         if user:
             auth.login(request, user)
-            return redirect('/post')
+            return HttpResponseRedirect(reverse('posts', args=[request.user.id]))
         else:
             loginError = 'Please enter corect Username or Password'
             message = {'loginError': loginError}
